90bc890efd9ac807a1bcf9e5bbc09d9467b1634b
jjimerod <jjimerod@ALC-1JHK3Z2.usersad.everis.int>
2021-01-05 16:21:18 +0100

    commit

--- DisneyPlus/src/edu/es/eoi/disney/CineMainApp.java
+++ DisneyPlus/src/edu/es/eoi/disney/CineMainApp.java
@@ -1,7 +1,5 @@
 package edu.es.eoi.disney;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -9,25 +7,40 @@ public class CineMainApp {
 
 	public static void main(String[] args) {
 
-		Cine cine=new Cine();
+		Cine cine = crearSesionCine();
+
+		Map<String, Persona> butacas = new TreeMap<String, Persona>();
+
+		generarKeySetButacas(butacas);
+
+		cine.setButacas(butacas);
+
+		// mi cine con mis butacas
+		System.out.println(butacas);
+		
+		//metodo generar y sentar personas
+	
+
+	}
+
+	private static Cine crearSesionCine() {
+
+		Cine cine = new Cine();
 		cine.setPelicula(new Pelicula("La Jungla de Cristal 1", 120.0, "1985", CategoriaEnum.ACCION));
 		cine.setPrecioEntrada(5.0);
-		
-		Map<String,Persona> butacas= new TreeMap<String, Persona>();
-			
-		for (int i = 1; i < 9; i++) {
-			String letra="ABCDEFGH";
-			for (int j = 0; j < 8; j++) {
-				String butaca=String.valueOf(i).concat(letra.substring(j,j+1));	
-				butacas.put(butaca, null);
-			}			
-		}
-		
-		cine.setButacas(butacas);		
-		
-		//mi cine con mis butacas		
-		System.out.println(butacas);
 
+		return cine;
+	}
+
+	private static void generarKeySetButacas(Map<String, Persona> butacas) {
+
+		for (int i = 1; i < 9; i++) {
+			String letra = "ABCDEFGH";
+			for (int j = 0; j < 8; j++) {
+				String butaca = String.valueOf(i).concat(letra.substring(j, j + 1));
+				butacas.put(butaca, null);
+			}
+		}
 	}
 
 }
