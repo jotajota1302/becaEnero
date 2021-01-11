e396548e41b7a618160a9e8d78eed4ef9eb73abc
jjimerod <jjimerod@ALC-1JHK3Z2.usersad.everis.int>
2021-01-11 20:26:07 +0100

    commit

--- gastroDatesParent/gastroDatesData/src/main/java/edu/es/eoi/repository/PersonRepositoryExcelImpl.java
+++ gastroDatesParent/gastroDatesData/src/main/java/edu/es/eoi/repository/PersonRepositoryExcelImpl.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
