75cad480e78736cd166b7ff8e5db73c87b575f7b
jjimerod <jjimerod@ALC-1JHK3Z2.usersad.everis.int>
2021-01-12 17:38:09 +0100

    commit

---
+++ gastroDates/src/main/java/edu/es/eoi/repository/RecipeRepositoryJDBCImpl.java
@@ -0,0 +1,45 @@
+package edu.es.eoi.repository;
+
+import java.util.Collection;
+
+import edu.es.eoi.entity.Recipe;
+
+public class RecipeRepositoryJDBCImpl implements RecipeRepository {
+
+	@Override
+	public void save(Recipe e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Recipe read(String sf) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Recipe e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Recipe update(Recipe e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Recipe> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Recipe> readRecipesByUser(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- gastroDates/src/main/java/edu/es/eoi/service/RecipeServiceImpl.java
+++ gastroDates/src/main/java/edu/es/eoi/service/RecipeServiceImpl.java
@@ -4,14 +4,14 @@ import java.util.Collection;
 
 import edu.es.eoi.entity.Recipe;
 import edu.es.eoi.repository.RecipeRepository;
-import edu.es.eoi.repository.RecipeRepositoryMemoryImpl;
+import edu.es.eoi.repository.RecipeRepositoryJDBCImpl;
 
 public class RecipeServiceImpl implements RecipeService {
 
 	@Override
 	public Collection<Recipe> readAll() {
 
-		RecipeRepository repository= new RecipeRepositoryMemoryImpl();
+		RecipeRepository repository= new RecipeRepositoryJDBCImpl();
 		return repository.readAll();		
 		
 	}
