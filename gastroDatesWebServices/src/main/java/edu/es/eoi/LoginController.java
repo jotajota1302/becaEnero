a5f60e1f484064dcad5a857475d03f7fd26c1b09
jjimerod <jjimerod@ALC-1JHK3Z2.usersad.everis.int>
2021-01-15 18:54:33 +0100

    commit

--- gastroDatesWebServices/src/main/java/edu/es/eoi/LoginController.java
+++ gastroDatesWebServices/src/main/java/edu/es/eoi/LoginController.java
@@ -1,16 +1,15 @@
 package edu.es.eoi;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.poi.util.IOUtils;
 
 import edu.es.eoi.entity.User;
 import edu.es.eoi.service.LoginService;
@@ -25,12 +24,31 @@ public class LoginController extends HttpServlet {
 
 		String fichero = req.getParameter("filename");
 
-//		if (fichero.equals("usuarios.xlsx")) {
-//			File file = new File("C://Users/jjimerod/Desktop/usuarios.xlsx");
-//			resp.setContentType("text/plain");
-//			resp.setHeader("Content-disposition", "attachment; filename=usuarios.xlsx");
-//				
-//		}
+		if (null!=fichero&&fichero.equals("prueba.txt")) {
+
+			File file = new File("C://Users/jjimerod/Desktop/prueba.txt");
+			FileInputStream stream = new FileInputStream(file);
+
+			resp.setContentType("text/plain");
+			resp.setContentLength((int) file.length());
+
+			String headerKey = "Content-Disposition";
+			String headerValue = String.format("attachment; filename=\"%s\"", file.getName());
+			resp.setHeader(headerKey, headerValue);
+
+			OutputStream outStream = resp.getOutputStream();
+
+			byte[] buffer = new byte[(int) file.length()];
+			int bytesRead = -1;
+
+			while ((bytesRead = stream.read(buffer)) != -1) {
+				outStream.write(buffer, 0, bytesRead);
+			}
+
+			stream.close();
+			outStream.close();
+
+		}
 
 	}
 
