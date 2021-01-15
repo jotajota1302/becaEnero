5ee155473557565cc5dc6f17f3a9671e4b5d6641
jjimerod <jjimerod@ALC-1JHK3Z2.usersad.everis.int>
2021-01-15 16:28:27 +0100

    commit

---
+++ gastroDatesWebServices/src/main/java/edu/es/eoi/TestServlet.java
@@ -0,0 +1,28 @@
+package edu.es.eoi;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String dinero = req.getParameter("dinero");
+		String moneda= req.getParameter("moneda");
+		
+		
+				
+		PrintWriter out = resp.getWriter();
+		out.println("Dinero " +dinero+" "+moneda);
+
+	}
+
+}
